203e991078c702d36dfc0c3a5f7a87b4bf020c38
Binotto <binotto10@gmail.com>
2024-01-21 18:15:29 -0300

    Added RelayCommand

--- Dashboard/Dashboard/Model/MainModel.cs
+++ Dashboard/Dashboard/Model/MainModel.cs
@@ -2,61 +2,61 @@
 {
     public class MenuItems
     {
-        public string? MenuName { get; set; }
-        public string? MenuImage { get; set; }
+        public string MenuName { get; set; }
+        public string MenuImage { get; set; }
     }
 
     public class HomeItems
     {
-        public string? HomeName { get; set; }
-        public string? HomeImage { get; set; }
+        public string HomeName { get; set; }
+        public string HomeImage { get; set; }
     }
 
     public class PCItems
     {
-        public string? PCName { get; set; }
-        public string? PCImage { get; set; }
+        public string PCName { get; set; }
+        public string PCImage { get; set; }
     }
 
     public class DesktopItems
     {
-        public string? DesktopName { get; set; }
-        public string? DesktopImage { get; set; }
+        public string DesktopName { get; set; }
+        public string DesktopImage { get; set; }
     }
 
     public class DocumentItems
     {
-        public string? DocuementName { get; set; }
-        public string? DocumentImage { get; set; }
+        public string DocuementName { get; set; }
+        public string DocumentImage { get; set; }
     }
 
     public class DownloadItems
     {
-        public string? DownloadName { get; set; }
-        public string? DownloadImage { get; set; }
+        public string DownloadName { get; set; }
+        public string DownloadImage { get; set; }
     }
 
     public class PictureItems
     {
-        public string? PictureName { get; set; }
-        public string? PictureImage { get; set; }
+        public string PictureName { get; set; }
+        public string PictureImage { get; set; }
     }
 
     public class MusicItems
     {
-        public string? MusicName { get; set; }
-        public string? MusicImage { get; set; }
+        public string MusicName { get; set; }
+        public string MusicImage { get; set; }
     }
 
     public class MovieItems
     {
-        public string? MovieName { get; set; }
-        public string? MovieImage { get; set; }
+        public string MovieName { get; set; }
+        public string MovieImage { get; set; }
     }
 
-    public class MovieItems
+    public class TrashItems
     {
-        public string? MovieName { get; set; }
-        public string? MovieImage { get; set; }
+        public string TrashName { get; set; }
+        public string TrashImage { get; set; }
     }
 }
---
+++ Dashboard/Dashboard/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Dashboard
+{
+    /// <summary>
+    /// RelayCommand allow you to inject the command's logic via delegates passed into its constructor. This method
+    /// enables ViewModel classes to implement commands in a concise manner.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        #region Fields
+
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+        #endregion
+
+        #region Constructor
+
+        public RelayCommand(Action<object> execute)
+        {
+            this.execute = execute;
+            this.canExecute = null;
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        #endregion
+
+        /// <summary>
+        /// CanExecuteChanged delegates the event subscription to the Commandmanager.RequerySuggested event.
+        /// This ensures that the WPF commanding infra asks all RelayCommand objects if they can execute whenever
+        /// it asks the built-in commands.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
