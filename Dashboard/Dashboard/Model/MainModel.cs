195da73cd7820c6d111bf2a3d10d3040d2a94289
Binotto <binotto10@gmail.com>
2024-01-21 16:27:40 -0300

    Created a model

---
+++ Dashboard/Dashboard/Model/MainModel.cs
@@ -0,0 +1,62 @@
+﻿namespace Dashboard.Model
+{
+    public class MenuItems
+    {
+        public string? MenuName { get; set; }
+        public string? MenuImage { get; set; }
+    }
+
+    public class HomeItems
+    {
+        public string? HomeName { get; set; }
+        public string? HomeImage { get; set; }
+    }
+
+    public class PCItems
+    {
+        public string? PCName { get; set; }
+        public string? PCImage { get; set; }
+    }
+
+    public class DesktopItems
+    {
+        public string? DesktopName { get; set; }
+        public string? DesktopImage { get; set; }
+    }
+
+    public class DocumentItems
+    {
+        public string? DocuementName { get; set; }
+        public string? DocumentImage { get; set; }
+    }
+
+    public class DownloadItems
+    {
+        public string? DownloadName { get; set; }
+        public string? DownloadImage { get; set; }
+    }
+
+    public class PictureItems
+    {
+        public string? PictureName { get; set; }
+        public string? PictureImage { get; set; }
+    }
+
+    public class MusicItems
+    {
+        public string? MusicName { get; set; }
+        public string? MusicImage { get; set; }
+    }
+
+    public class MovieItems
+    {
+        public string? MovieName { get; set; }
+        public string? MovieImage { get; set; }
+    }
+
+    public class MovieItems
+    {
+        public string? MovieName { get; set; }
+        public string? MovieImage { get; set; }
+    }
+}
